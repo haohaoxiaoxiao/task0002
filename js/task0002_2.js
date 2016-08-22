1db1597207310a78a161aa282432b89d15a72bee
haohaoxiaoxiao <2226765961@qq.com>
2016-08-22 18:09:48 +0800

    Update task0002_2.js

--- js/task0002_2.js
+++ js/task0002_2.js
@@ -27,14 +27,30 @@ function countTime() {
 			inputDate = input.value.split("-"),
 			year = inputDate[0] - currentDate.getFullYear(),
 			month = inputDate[1] - 1 - currentDate.getMonth(),
-			day = inputDate[2] - currentDate.getDay(),
+			day = inputDate[2] - currentDate.getDate(),
 			days = year*365 + month*30 + day - 1,
 			hour = 23 - currentDate.getHours(),
 			minute = 59 - currentDate.getMinutes(),
-			second = 59 - currentDate.getSeconds();
+			second = 59 - currentDate.getSeconds(),
+			seconds = days*24*60*60+hour*60+minute*60+second;
+			console.log(year);
+			console.log(month);
+			console.log(day);
+			console.log(days);
+			console.log(seconds);
+		if (seconds == 0) {
+			var p2 =document.createElement(p);
+			p2.innerHTML = "时间到";
+			result.appendChild(p2);
+		} else if (seconds < 0) {
+			var p3 =document.createElement(p);
+			p3.innerHTML = "请输入未来时间";
+			result.appendChild(p3);
+		} else {
 		var p1 = document.createElement(p);
 		p1.innerHTML = '距离' + inputDate[0] + '年' + inputDate[1] + '月' + inputDate[2] + '日还有' + days + '天' + hour + '小时' + minute +'分钟' + second + '秒';
 		result.appendChild(p1);
+		}	
 
 	}
 }
